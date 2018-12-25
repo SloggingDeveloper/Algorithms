b2cbc0920f8a325a02dfa0d70e59e2c984436202
SloggingDeveloper <sunilganiger@gmail.com>
2018-12-24 23:17:26 -0600

    added comments

--- Js/src/DivideAndConquer/execute.js
+++ Js/src/DivideAndConquer/execute.js
@@ -1,9 +1,10 @@
 'use strict';
 
-
 const array = require('./maxSubArray.js');
+
+//max sub array
 var arr = [-2,-9,-2,1,-4];
-
+console.log('finding max sub array');
+console.log(arr);
 const result =array.calculateMaxSubArray(arr, 0, 4);
-
 console.log(result);
