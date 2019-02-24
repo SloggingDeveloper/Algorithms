224ab2561c21b63f846d8ad3af8c1b2b226c46cd
SloggingDeveloper <sunilganiger@gmail.com>
2019-02-24 14:38:06 -0600

    added counting algorithm

---
+++ Js/src/Sorting/counting.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+(function(){
+   
+    const sort = function(arr){
+     let sortedArr = [], makeShiftArr = [];
+
+     for(let i = 0 ;i<arr.length; i++){
+         makeShiftArr[arr[i]] = isNaN( makeShiftArr[arr[i]]) ? 1 :  makeShiftArr[arr[i]] + 1;
+     }
+
+      if(isNaN(makeShiftArr[0]))
+       makeShiftArr[0] = 0;
+
+     for(let j = 1; j < makeShiftArr.length; j++){
+       makeShiftArr[j] = isNaN(makeShiftArr[j]) ? makeShiftArr[j-1] : makeShiftArr[j] + makeShiftArr[j-1]
+     }
+       
+     for(let k = 0; k < arr.length; k++){
+         sortedArr[makeShiftArr[arr[k]] - 1] = arr[k];
+     }
+       return sortedArr;
+    };
+
+    module.exports = {sort};
+})();
--- Js/src/Sorting/test.js
+++ Js/src/Sorting/test.js
@@ -2,7 +2,11 @@
 
 const heap = require('./heap');
 const quickSort = require('./quickSort');
+const countingSort = require('./counting');
 
-console.log('heapsort ' + heap.sort([14,3,8,6,9,1,22, 11]));
-console.log('quicksort ' + quickSort.sort([14,3,8,6,9,1,22, 11], 0, 7));
-console.log('quicksort randomized ' + quickSort.sort([14,3,8,6,9,1,22, 11], 0, 7));
+// console.log('heapsort ' + heap.sort([14,3,8,6,9,1,22, 11]));
+// console.log('quicksort ' + quickSort.sort([14,3,8,6,9,1,22, 11], 0, 7));
+// console.log('quicksort randomized ' + quickSort.sort([14,3,8,6,9,1,22, 11], 0, 7));
+
+let result = countingSort.sort([14,3,8,6,9,1,22, 11]);
+console.log(result);
