06f35d1c12c9d30c7bdda9fc47b500e87c01ef22
SloggingDeveloper <sunilganiger@gmail.com>
2019-02-24 23:56:41 -0600

    added radix sort

---
+++ Js/src/Sorting/radix.js
@@ -0,0 +1,57 @@
+'use strict';
+
+(function(){
+   
+    const countSortDigit = function(arr, placeHolder){
+      const modulusof = Math.pow(10,placeHolder);
+      const divisor = Math.pow(10,placeHolder-1);
+      const swapTempArr =[]
+      for(let j = 0; j < 10 ;j++){
+        swapTempArr.push({position: 0, frequency: 0});
+      }
+
+      const sortedArr = [];
+      const getDigit = (element) =>  Math.floor((element%modulusof)/divisor);
+      for(let i = 0; i < arr.length; i++){
+          let digit = getDigit(arr[i]);
+          swapTempArr[digit].frequency += 1;
+          swapTempArr[digit].position += 1;
+      }
+
+      for(let j = 1; j < swapTempArr.length; j++){
+        swapTempArr[j].position += swapTempArr[j-1].position;
+      }
+
+      for(let i = 0; i < arr.length; i++){
+        let digit = getDigit(arr[i]);        
+        sortedArr[swapTempArr[digit].position - swapTempArr[digit].frequency] = arr[i];
+        swapTempArr[digit].frequency -= 1;
+    }    
+    
+    return sortedArr;
+    };
+
+    const getNumberOfDigits = function(element){
+     let totalDigits = 0;
+     if(element === 0)
+     return 1;
+
+     while(element > 0){
+        element = Math.floor(element/10)
+        totalDigits += 1;
+     }
+
+     return totalDigits;
+    };
+
+    const sort = function(arr){
+        let maxDigits = getNumberOfDigits(Math.max(...arr));
+        for(let i = 1; i <=maxDigits; i++){            
+          arr = countSortDigit(arr, i);
+        }
+
+       return arr;
+    } 
+
+    module.exports = {sort};
+})();
--- Js/src/Sorting/test.js
+++ Js/src/Sorting/test.js
@@ -3,10 +3,12 @@
 const heap = require('./heap');
 const quickSort = require('./quickSort');
 const countingSort = require('./counting');
-
+const radix = require('./radix');
 // console.log('heapsort ' + heap.sort([14,3,8,6,9,1,22, 11]));
 // console.log('quicksort ' + quickSort.sort([14,3,8,6,9,1,22, 11], 0, 7));
 // console.log('quicksort randomized ' + quickSort.sort([14,3,8,6,9,1,22, 11], 0, 7));
 
-let result = countingSort.sort([14,3,22,8,6,9,9,1,22, 11]);
-console.log(result);
+// let result = countingSort.sort([14,3,22,8,6,9,9,1,22, 11]);
+// console.log(result);
+
+console.log('radix: ' + radix.sort([11, 123, 760, 511]));
