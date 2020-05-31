52637298b2055c505fb07e20c014239130c80d5f
SloggingDeveloper <sunilganiger@gmail.com>
2020-05-31 10:21:45 -0500

    algorithms in progress

--- Js/src/General/combination.js
+++ Js/src/General/combination.js
@@ -1,5 +1,5 @@
 (function(){
-
+    var phonePad = ["","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
     function combination(input, group, ignoreDuplicate){       
         var result = [];
         console.log(group);
@@ -21,5 +21,33 @@
         return result;
     }
 
+    function phoneLetterCombination(input){
+        var combinations = combination(input, input.length);
+        var finalCombinations = [];
+        for(var i = 0; i.length < finalCombinations.length; i++){
+            var arr = combinations[i].split('');
+            var keypad = [];
+            for(var j = 0; j < arr.length; j++){
+                keypad.push(arr[j])
+            }
+            finalCombinations.push(phoneLetterPickOneEachFromGroup(keypad, 0,"",[]));
+        }  
+        return finalCombinations;
+    }
+
+    function phoneLetterPickOneEachFromGroup(inputGroup, currentGroupIndex, result, allCombinations){
+         if(currentGroupIndex === inputGroup.length)
+         return result;
+
+         var split = inputGroup[currentGroupIndex].split('');
+         for(var i = 0; i< split.length; i++){           
+            allCombinations.push(phoneLetterPickOneEachFromGroup(inputGroup, currentGroupIndex + 1, result+split[i]));
+         }
+        return allCombinations;
+    }
+
+
+
 module.exports = combination;    
 })();
+
--- Js/src/General/test.js
+++ Js/src/General/test.js
@@ -1,7 +1,4 @@
-'use strict';
-const combinatin = require('./combination');
-var map = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
-
-
-console.log(combinatin("abc",2, true));
-
+// 'use strict';
+// const findAnagrams = require('./Anagrams');
+// const primes = require('./GeneratePrimes');
+// console.log(findAnagrams(["rob","bor","ad","bc"]));
