eadcca8bbb25382466b7d1be7214baec300131cb
SloggingDeveloper <sunilganiger@gmail.com>
2020-12-26 20:16:29 -0600

    interval merge algorithm- WIP

--- Js/src/General/test.js
+++ Js/src/General/test.js
@@ -1,2 +1,2 @@
-const match = require('./subStringKPM');
-console.log(match("abgrexmmabgmxabgtxnztxn", "abgmxabgtxn"));
+const merge = require('./intervalMerge');
+console.log(merge([[1,3],[6,8],[10,12],[20,22]], [7,11]));
