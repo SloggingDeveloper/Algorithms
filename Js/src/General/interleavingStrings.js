66091eb38cae86e5b86fc0934863b2999866dd06
SloggingDeveloper <sunilganiger@gmail.com>
2021-01-11 10:24:29 -0600

    interleaving strs

--- Js/src/General/interleavingStrings.js
+++ Js/src/General/interleavingStrings.js
@@ -1,31 +1,55 @@
 (function(){
  'use strict';
+ //Problem Type: Hard
 
- const contains = function(mainStr, subStr1, subStr2){
+ const contains = function(s1, s2, s3){
+   
  
-     return containsStr(mainStr, subStr1) && containsStr(mainStr, subStr2);    
+    if((s1.length + s2.length) !== s3.length)
+      return false;
 
+    const matrix = [];
+
+    for(let i = 0; i <= s2.length;i++){
+        matrix[i] = [];
+        for(let j = 0; j <= s1.length; j++){
+            matrix[i][j] = false;
+        }
+    }
+
+    matrix[0][0] = true;
+    
+    for(let i = 0; i <= s2.length;i++){       
+        for(let j = 0; j <= s1.length; j++){
+
+            if(i === 0 && j===0){
+                continue;
+            }
+
+            if(i === 0){
+                if(s3[j-1] === s2[j-1]){
+                 matrix[i][j] = matrix[i][j-1];
+                }
+                continue;
+            }
+
+            if(j === 0){
+                if(s3[i-1] === s1[i-1]){
+                    matrix[i][j] = matrix[i-1][j];
+                }
+                continue;
+            }
+            
+            if(s3[i+j-1] === s2[j-1] || s3[i+j-1] === s1[1-1]){
+                matrix[i][j] = matrix[i-1][j] || matrix[i][j-1];
+            }
+
+        }
+    }
+
+    return  matrix[s1.length][s2.length];
+    
  };
-
- const containsStr = function(mainStr, subStr){
-
-    if(typeof(mainStr) === 'undefined' || subStr === '' || typeof(subStr) === 'undefined'){
-        return false;
-    }
-
-    let subStrIndex = 0;
-
-    for(let i = 0; i < mainStr.length; i++){
-        
-        if(subStrIndex === subStr.length)
-         break;
-
-        if(mainStr[i] === subStr[subStrIndex])
-         subStrIndex++;
-    }
-
-    return  subStrIndex === subStr.length;
- }
 
  module.exports = contains;
 })();
