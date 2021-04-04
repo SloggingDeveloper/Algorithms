37b6ee24669c5811a98a3e95042f5f7366554d8a
sunil ganiger <Sunil_Ganiger@Dell.com>
2021-04-04 18:31:14 -0500

    completed min window substr

--- Js/src/General/minimumWindowSubstring.js
+++ Js/src/General/minimumWindowSubstring.js
@@ -5,25 +5,25 @@
 
         let i = 0, j = 0,
             minimumLength = Number.MAX_VALUE,
-            start = 0, end = 0, minWindowMem,
-            prevI = -1,
+            start = 0, end = 0, minWindowMem = {},
             prevJ = -1;
 
         for (let item of substr) {
             minWindowMem[item] = 0;
         }
 
-        while (i < str.length && j < str.length && i <= j) {
-
-            if (typeof minWindowMem[str[i]] !== 'undefined' && prevI !== i) {
+        while (i < str.length && j < str.length && i <= j) {          
+            if (typeof minWindowMem[str[i]] !== 'undefined' && minWindowMem[str[i]] <= 0) {
                 minWindowMem[str[i]] += 1;
             }
 
-            if (typeof minWindowMem[str[j]] !== 'undefined' && prevJ !== j) {
+            if (i !== j && typeof minWindowMem[str[j]] !== 'undefined' && prevJ !== j) {
                 minWindowMem[str[j]] += 1;
+                prevJ = j;
             }
 
-            if (checkAllCharactersFound(minWindowMem)) {
+            if ((j - i + 1) >= substr.length && checkAllCharactersFound(minWindowMem)) {
+
                 if ((j - i + 1) < minimumLength) {
                     minimumLength = j - i + 1;
                     start = i;
@@ -31,21 +31,26 @@
                 }
                 minWindowMem[str[i]] -= 1;
                 i++;
+                continue;
             }
 
 
-            else if (typeof minWindowMem[str[i]] !== 'undefined') {
+            if (typeof minWindowMem[str[i]] !== 'undefined') {
                 j++;
+                continue;
             }
-            
-            else {
+
+            if (typeof minWindowMem[str[j]] !== 'undefined' && i < j) {
                 i++;
-                j++;
+                continue;
             }
 
-            prevI = i;
-            prevJ = j;
-        };
+            j++;
+            i++;           
+
+        }
+
+        return str.substr(start, end - start + 1);
     }
 
     const checkAllCharactersFound = function (minWindowMem) {
