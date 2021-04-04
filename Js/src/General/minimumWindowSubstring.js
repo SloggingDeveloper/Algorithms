5fd7caa32e18b71df90fcd7994d30095d7a40263
sunil ganiger <Sunil_Ganiger@Dell.com>
2021-04-04 13:39:25 -0500

    substringAlgo

--- Js/src/General/minimumWindowSubstring.js
+++ Js/src/General/minimumWindowSubstring.js
@@ -3,50 +3,58 @@
 
     const getMinimumWindow = function (str, substr) {
 
-        let left = 0, right = 0, minimumLength = Number.MAX_VALUE, start = 0, end = 0, minWindowMem;
+        let i = 0, j = 0,
+            minimumLength = Number.MAX_VALUE,
+            start = 0, end = 0, minWindowMem,
+            prevI = -1,
+            prevJ = -1;
+
         for (let item of substr) {
             minWindowMem[item] = 0;
         }
 
         while (i < str.length && j < str.length && i <= j) {
 
-            if (typeof minWindowMem[str[i]] !== 'undefined' && typeof minWindowMem[str[j]] !== 'undefined' & i != j) {
-                let found = true;
+            if (typeof minWindowMem[str[i]] !== 'undefined' && prevI !== i) {
+                minWindowMem[str[i]] += 1;
+            }
 
-                for (let item in minWindowMem) {
-                    if (minWindowMem.hasOwnProperty(item)) {
+            if (typeof minWindowMem[str[j]] !== 'undefined' && prevJ !== j) {
+                minWindowMem[str[j]] += 1;
+            }
 
-                        if (minWindowMem[item] <= 0) {
-                            found = false;
-                            break;
-                        }
-                    }
+            if (checkAllCharactersFound(minWindowMem)) {
+                if ((j - i + 1) < minimumLength) {
+                    minimumLength = j - i + 1;
+                    start = i;
+                    end = j;
                 }
-
-                if (found && (j-i + 1) < minimumLength) {
-                   minimumLength = j-i + 1;
-                   start = i;
-                   end = j;
-                   minWindowMem[str[i]] -= 1;
-                   i++;
-                }
+                minWindowMem[str[i]] -= 1;
+                i++;
+            }
 
 
-                else if (typeof minWindowMem[str[i]] !== 'undefined') {
-                    minWindowMem[str[i]] += 1;
-                    j++;
-                }
-                else if (typeof minWindowMem[str[j]] !== 'undefined') {
-                    minWindowMem[str[j]] += 1;
-                    if (typeof minWindowMem[str[i]] !== 'undefined')
-                        j++;
-                    else {
-                        i++;
-                    }
-                }
+            else if (typeof minWindowMem[str[i]] !== 'undefined') {
+                j++;
+            }
+            
+            else {
+                i++;
+                j++;
+            }
 
+            prevI = i;
+            prevJ = j;
+        };
+    }
 
-            };
+    const checkAllCharactersFound = function (minWindowMem) {
+        for (let item in minWindowMem) {
+            if (minWindowMem.hasOwnProperty(item) && minWindowMem[item] <= 0)
+                return false;
+        }
+        return true;
+    };
 
-            module.exports = getMinimumWindow;
-        }) ();
+    module.exports = getMinimumWindow;
+})();
